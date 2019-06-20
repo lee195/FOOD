7e396dd2172d321bd3b90f5393dc08b86834bac9
jisu <jisu_lee@hotmail.de>
2019-06-20 11:58:33 +0200

    mem is now a capped hash container

--- BlockChain.py
+++ BlockChain.py
@@ -9,15 +9,36 @@ import hashlib, Block, random
 
 random.seed(0)
 
-memory = []
+
+class CappedHash:
+    """Hash container of fixed capacity"""
+
+    def __init__(self, data, capacity):
+        self.cap = capacity
+        if len(data) > capacity:
+            self.data = dict(enumerate(data[-capacity:]))
+        else:
+            self.data = dict(enumerate(data))
+        self.ptr = len(data) % capacity
+
+    def __repr__(self):
+        return self.data.__str__()
+
+    def add_entry(self, entry):
+        self.data[self.ptr % self.cap] = entry
+        self.ptr += 1
+
+    def get_nth_last_entry(self, index):
+        return self.data[(self.ptr - index) % self.cap]
 
 
 def generate_genesis():
     return Block.Block(['Genesis tx'], 0)
 
 
+memory = CappedHash([generate_genesis()], 10)
+
 if __name__ == "__main__":
     print('Blockchain Simulation: Distributed Object Ordering with Fairness\n')
-    memory.append(generate_genesis())
-    for entry in memory:
-        print(str(entry) + '\n')
+    for k, v in memory.data.items():
+        print(str(k) + '\n' + str(v))
